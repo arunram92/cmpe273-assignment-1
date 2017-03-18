90cb71f4c0de8b223bd87ff2cb62e4ad134156fe
Y.Arun Ramanathan <y.arunram@gmail.com>
2017-03-17 23:37:30 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,65 +1,47 @@
 from flask import Flask
 from github import Github
 import base64
-import json
 import sys
 
 app = Flask(__name__)
 
 @app.route('/')
-def devenv():
-    output = getDataFromGit("dev")
+def home():
+    output = "Please use the format /v1/your file name"
     return output
 
-@app.route('/test')
-def testenv():
-    output = getDataFromGit("test")
+@app.route('/v1/')
+def v1():
+    output = "Please use the format /v1/your file name"
     return output
 
-def getDataFromGit(env):
+@app.route('/v1/<fileName>')
+def devenv(fileName):
+    output = getDataFromGit(fileName)
+    return output
+
+def getDataFromGit(fileName):
 
     commandLineInput = ((sys.argv[1]).split("/"))
     commandLineInput.reverse()
     repositoryName = commandLineInput[0]
     userName = commandLineInput[1]
-
+    print userName
+    print repositoryName
     try:
 
         repo = Github().get_user(userName).get_repo(repositoryName)
 
-        fileName = env + "-config.json"
         output = ""
         try:
             decodedJsonString = base64.b64decode(repo.get_contents(fileName).content)
-            config = json.loads(decodedJsonString)
-            try:
-                first_number = config["first_number"]
-                second_number = config["second_number"]
-                operation = config["operation"]
-
-                output += "message : " + config["welcome_message"] + "<br />"
-                output += "first_number : " + str(first_number) + "<br />"
-                output += "second_number : " + str(second_number) + "<br />"
-                output += "operation : '" + operation + "'<br />"
-                output += "result : " + str(operations(first_number, second_number, operation)) + "<br />"
-            except:
-                output = "One or more values needed for calculator app was not found in the json file.<br/> Required Values : welcome_message,first_number,second_number,operation"
+            output = decodedJsonString
         except:
-            output = "cannot find the config json in the repository"
+            output = "cannot find the config file '" +fileName+ "' in the repository '"+repositoryName+"'"
     except:
         output = "Cannot find the github repository"
 
     return output
-
-def operations(x,y,op):
-    if(op=='+'):
-        return x+y
-    elif(op=='-'):
-        return x-y
-    elif (op == '*'):
-        return x*y
-    else:
-        return x/y
 
 if __name__ == '__main__':
     app.run()
